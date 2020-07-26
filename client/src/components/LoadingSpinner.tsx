77e766ddf3cc5673573d2e0e6e27b6fd3cc74cee
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2020-07-26 10:13:16 -0500

    [DESIGN] Swap loading spinner

--- client/src/components/LoadingSpinner.tsx
+++ client/src/components/LoadingSpinner.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 
 export const LoadingSpinner = (): JSX.Element => {
   return (
-    <div className="la-cog la-dark la-3x">
+    <div className="la-ball-pulse-sync la-dark la-3x">
+      <div></div>
+      <div></div>
       <div></div>
     </div>
   );
