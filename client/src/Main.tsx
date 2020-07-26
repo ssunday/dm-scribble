2e576930e4a036214c163b1f3304d6bcee84bbdf
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2020-07-26 10:21:50 -0500

    [SITE] Add not found page

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -3,6 +3,7 @@ import { Route, Router, Switch } from 'react-router';
 import { createBrowserHistory } from 'history';
 import { CampaignRoutes } from './campaigns/CampaignRoutes';
 import { Home } from './app/Home';
+import { NotFound } from './app/NotFound';
 import { Layout } from './app/Layout';
 
 import './assets/stylesheets/main.scss';
@@ -13,7 +14,9 @@ export const Main = (): JSX.Element => {
       <Switch>
         <Layout>
           <Route exact path="/" component={Home} />
+          <Route exact path="/not-found" component={NotFound} />
           <Route path="/campaigns" component={CampaignRoutes} />
+          <Route exact path="/*" component={NotFound} />
         </Layout>
       </Switch>
     </Router>
---
+++ client/src/app/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const NotFound = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+    </div>
+  );
+};
